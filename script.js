a443e49e1eb0d14c2c7f59943ca6d18b4a301ac8
Karamraj Singh Anand <karamraj.anand@outlook.com>
2024-01-02 23:47:26 +0530

    Done

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
-function calculateTax() {
+function calculateTax(type) {
     var income = document.getElementById("income").value;
     var resultElement = document.getElementById("result");
 
-    if (income <= 1000000) {
-        var tax = (income * 15) / 100;
-        resultElement.innerHTML = "The Tax to be collected: " + tax;
-    } else if (income <= 1500000) {
-        var tax = (income * 20) / 100;
-        resultElement.innerHTML = "The Tax to be collected: " + tax;
-    } else if (income <= 2000000) {
-        var tax = (income * 25) / 100;
-        resultElement.innerHTML = "The Tax to be collected: " + tax;
-    } else if (income > 2000000) {
-        var tax = (income * 30) / 100;
-        resultElement.innerHTML = "The Tax to be collected: " + tax;
+    if (type === 'weekly') {
+        if (income <= 250000) {
+            var tax = (income * 3.75) / 100;
+            resultElement.innerHTML = "Weekly Tax: " + tax;
+        } else if (income <= 375000) {
+            var tax = (income * 5) / 100;
+            resultElement.innerHTML = "Weekly Tax: " + tax;
+        } else if (income <= 500000) {
+            var tax = (income * 6.25) / 100;
+            resultElement.innerHTML = "Weekly Tax: " + tax;
+        } else {
+            var tax = (income * 7.5) / 100;
+            resultElement.innerHTML = "Weekly Tax: " + tax;
+        }
+    } else if (type === 'monthly') {
+        if (income <= 1000000) {
+            var tax = (income * 15) / 100;
+            resultElement.innerHTML = "Monthly Tax: " + tax;
+        } else if (income <= 1500000) {
+            var tax = (income * 20) / 100;
+            resultElement.innerHTML = "Monthly Tax: " + tax;
+        } else if (income <= 2000000) {
+            var tax = (income * 25) / 100;
+            resultElement.innerHTML = "Monthly Tax: " + tax;
+        } else if (income > 2000000) {
+            var tax = (income * 30) / 100;
+            resultElement.innerHTML = "Monthly Tax: " + tax;
+        }
     }
 }
